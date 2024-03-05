cbee4c676e9ac97ff992a5782a85013902e86b63
agkhushi <127933268+agkhushi@users.noreply.github.com>
2024-03-05 10:19:00 +0530

    Time: 82 ms (81.15%) | Memory: 95.9 MB (25.10%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,9 +4,9 @@ public:
         int single_day=prices[0];
         int profit=0;
         for(int i=1;i<prices.size();i++){
-            int different_day=prices[i]-single_day;
             single_day=min(single_day,prices[i]);
-            profit=max(profit,different_day);
+            int differrent_day=prices[i]-single_day;
+            profit=max(profit,differrent_day);
         }
         return profit;
     }
