70bdffcecbd59fb73ab4da18886f2021e169378e
agkhushi <127933268+agkhushi@users.noreply.github.com>
2024-03-13 15:42:15 +0530

    Time: 3 ms (51.39%) | Memory: 7.2 MB (73.82%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int l = 1;
+        int r = n;
+        int sum=(n*(n+1))/2;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            int firstsum=(mid*(mid+1))/2;
+            int endsum=sum-firstsum+mid;
+            if(firstsum==endsum){
+                return mid;
+            }
+            else if(firstsum<endsum){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+
+        }
+        return -1;
+    }
+};
