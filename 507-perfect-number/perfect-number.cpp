69e3677582ab89f3f9fd4611bd76193458741056
agkhushi <127933268+agkhushi@users.noreply.github.com>
2024-04-21 15:57:13 +0530

    Time: 2084 ms (26.34%) | Memory: 7.1 MB (59.09%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -3,7 +3,7 @@ public:
     bool checkPerfectNumber(int num) {
         int sum=0;
         int i=1;
-        while(i<=num/2){
+        while(i<num){
             if(num%i==0){
                 sum+=i;
             }
