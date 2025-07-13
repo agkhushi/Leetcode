7c709bdf56ef45e5f8f345d5c1ee041200a0f5f5
Khushi Agarwal <127933268+agkhushi@users.noreply.github.com>
2025-07-13 14:20:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (86.43%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> position;
-        int n=nums.size()-1;
-        int fst=-1;
-        int lst=-1;
-        int l=0;
-        int r=n;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if (target==nums[mid]){
-                fst=mid;
-                r=mid-1;
-            }
-            else if (target>nums[mid]){
-                l=mid+1;
-            }
-            else{
-                r=mid-1;
+        int n=nums.size();
+        vector<int> ans;
+        int first=-1;
+        int last=-1;
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                first = mid;
+                high = mid - 1;
             }
         }
-        l=0;
-        r=n;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if (target==nums[mid]){
-                lst=mid;
-                l=mid+1;
-            }
-            else if (target>nums[mid]){
-                l=mid+1;
-            }
-            else{
-                r=mid-1;
+         low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                last = mid;
+                low = mid + 1;
             }
         }
-        position.push_back(fst);
-        position.push_back(lst);
-        return position;
+        return {first,last};
     }
 };
