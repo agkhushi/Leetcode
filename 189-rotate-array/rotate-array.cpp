073f0d7933cbe4108dcfa3911309e28279defcb7
Khushi Agarwal <127933268+agkhushi@users.noreply.github.com>
2025-12-11 21:15:18 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (98.63%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,8 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k=k%n;
-        reverse(nums.begin(),nums.end());// reverse the whole array 
-        reverse(nums.begin(),nums.begin()+k); //reverse the first k part jisse ehla ans miljaye pehla part 
-        reverse(nums.begin()+k,nums.end());//this reverses the rest of the array jisse wo sort hojaye 
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
